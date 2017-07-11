d7878b8198ed73570b357a0768d6c0904cf3a9d8
fhajdar1989 <fuadhajdarevic@gmail.com>
2017-07-11 18:43:45 +0200

    'lala'

---
+++ app/src/main/java/com/example/user/elpaso/DetailsActivity.java
@@ -0,0 +1,9 @@
+package com.example.user.elpaso;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by User on 26.12.2016.
+ */
+public class DetailsActivity extends AppCompatActivity {
+}
